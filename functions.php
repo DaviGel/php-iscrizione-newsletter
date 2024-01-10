343be7df595ee4e2f7134a162e6bac84302d2b3d
DaviGel <geloso.davide@gmail.com>
2024-01-10 16:04:11 +0100

    feat: milestone 2

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+  $email = $_GET['email'] ?? '';
+  function isValidEmail($email) {
+    $pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
+    if (preg_match($pattern, trim($email))) return true; 
+    else return false;
+  }
--- index.php
+++ index.php
@@ -19,14 +19,7 @@
         </div>
         <button type="submit" class="btn btn-primary">Invia</button>
       </form>
-      <?php
-        $email = $_GET['email'] ?? '';
-        function isValidEmail($email) {
-          $pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
-          if (preg_match($pattern, trim($email))) return true; 
-          else return false;
-        }
-      ?>
+      <?php include __DIR__ . "/functions.php"; ?>
       <?php if($email !== '' && isValidEmail($email) === true) : ?>
       <div class="alert alert-success mt-3" role="alert">
         L'email inserita è valida!
